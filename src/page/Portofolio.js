60576c5d593f6e567a4985256fafb734a5a961f5
denihida1216 <denihida1216@gmail.com>
2020-06-20 12:04:42 +0700

    tambah static.config.js

---
+++ src/page/Portofolio.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Portofolio extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Portofolio</h2>
+        <p>Email: info@email.com <br />
+           Phone: 002938293089
+</p>
+      </div>
+    );
+  }
+}
+ 
+export default Portofolio;
---
+++ static.config.js
@@ -0,0 +1,4 @@
+export default {
+    siteRoot: "https://denihida1216.github.io",
+    basePath: "website",
+}
