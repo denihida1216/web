83850a66c4a23c31695210ab69c842cabe488099
denihida1216 <denihida1216@gmail.com>
2020-06-20 14:24:06 +0700

    Updates

---
+++ precache-manifest.2e40512ea050cdb18c725d9aa34c0a17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd75b7927d226ccf671aba073d0f5cd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7edecbb7e403246ac52",
+    "url": "/static/css/main.0f904f23.chunk.css"
+  },
+  {
+    "revision": "68377b78f6aa7b96e741",
+    "url": "/static/js/2.7a5ec9d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a5ec9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7edecbb7e403246ac52",
+    "url": "/static/js/main.a91e4f00.chunk.js"
+  },
+  {
+    "revision": "233dea908d4de0135036",
+    "url": "/static/js/runtime-main.c8273c4c.js"
+  }
+]);
