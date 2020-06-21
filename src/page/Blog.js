f8269923de0c124aa075e396060c27ccaf7f263e
denihida1216 <denihida1216@gmail.com>
2020-06-21 09:40:05 +0700

    commit minggu 1

--- src/page/Blog.js
+++ src/page/Blog.js
@@ -5,10 +5,7 @@ class Blog extends Component {
     return (
       <div>
         <h2>Blog</h2>
-        <p>Halaman ini berisi project yang pernah dan sedang saya kerjakan. 
-          Ada yang bersifat side project dan ada juga yang serius. 
-          Untuk project private, saya tidak cantumkan di sini. 
-          Untuk project yang lainnya bisa ditemukan di <a href="github.com/denihida1216">Github</a>.</p>
+        <p>Halaman blog</p>
       </div>
     );
   }
