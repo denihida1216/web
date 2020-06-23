b0008e61e6ef437c3aa53f6fd197aedb8bc1abe6
denihida1216 <denihida1216@gmail.com>
2020-06-23 23:38:26 +0700

    updates

--- src/page/About.js
+++ src/page/About.js
@@ -4,8 +4,8 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 const MyImage = ({ image }) => (
   <LazyLoadImage
     alt="profil"
-    height="250px"
-    width="250px"
+    height="300px"
+    width="300px"
     effect="blur"
     src="./img/about/profil.jpg" />
 );
@@ -16,30 +16,30 @@ class About extends Component {
       <div>
         <h1>About</h1>
         <div className="row">
-        <div className="col-md-12">
-          <MyImage />
-          <p></p>
-        </div>
-        <div className="col-md-12">
-          <p>
-            Halo, saya Deni Hidayat, Sering dipanggil Deden (di rumah) dan Deni (di kampus).
+          <div className="col-md-12">
+            <MyImage />
+            <p></p>
+          </div>
+          <div className="col-md-12">
+            <p>
+              Halo, saya Deni Hidayat, Sering dipanggil Deden (di rumah) dan Deni (di kampus).
           </p>
-          <p>
-            Saat ini, Pekerjaan saya Freelance.
-            Pernah Menempuh Pendidikan di Universitas Bale Bandung Mengambil Program Studi Teknik Informatika.
+            <p>
+              Saat ini, Pekerjaan saya Freelance.
+              Pernah Menempuh Pendidikan di Universitas Bale Bandung Mengambil Program Studi Teknik Informatika.
           </p>
-          <p>
-            Kontak yang bisa dihubungi:
+            <p>
+              Kontak yang bisa dihubungi:
           </p>
-          <p>
-            Email: <a href="mailto:denihida1216@gmail.com">denihida1216@gmail.com</a><br />
+            <p>
+              Email: <a href="mailto:denihida1216@gmail.com">denihida1216@gmail.com</a><br />
             WA / Telegram : <a href="tel:+6285721661555">+6285721661555</a><br />
             Github : <a href="https://github.com/denihida1216">https://github.com/denihida1216</a><br />
             Linked : <a href="https://linked.com/denihida1216">https://linked.com/denihida1216</a><br />
             Instagram : <a href="https://instagram.com/denihida1216">https://instagram.com/denihida1216</a><br />
             Facebook : <a href="https://facebook.com/denihida1216">https://facebook.com/denihida1216</a>
-          </p>
-        </div>
+            </p>
+          </div>
         </div>
       </div>
     );
